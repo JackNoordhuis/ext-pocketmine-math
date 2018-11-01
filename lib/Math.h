00e5f8c4b18d3f9dbe401c3787d8fac599a6d685
Jack Noordhuis <daswaggamer@gmail.com>
2018-11-01 15:55:34 +1100

    Implement math class

--- lib/Math.h
+++ lib/Math.h
@@ -5,6 +5,9 @@
 #ifndef EXT_POCKETMINE_MATH_MATH_H
 #define EXT_POCKETMINE_MATH_MATH_H
 
+#include <cmath>
+#include <vector>
+
 class Math {
 public:
 
@@ -13,14 +16,20 @@ public:
      *
      * @return
      */
-    static int floorFloat(double n);
+    static int floorFloat(double n) {
+        int i = (int)n;
+        return n >= i ? i : i - 1;
+    }
 
     /**
      * @param n
      *
      * @return
      */
-    static int ceilFloat(double n);
+    static int ceilFloat(double n) {
+        int i = (int)n;
+        return n <= i ? i : i + 1;
+    }
 
     /***
      * Solves a quadratic equation with the given coefficients and returns an array of up to two solutions.
@@ -31,7 +40,22 @@ public:
      *
      * @return pointer to an array of possible solutions.
      */
-    static double *solveQuadratic(double a, double b, double c);
+    static std::vector<double> solveQuadratic(double a, double b, double c) {
+        double discriminant = pow(b, 2) - 4 * a * c;
+        if(discriminant > 0) { // 2 real roots
+            double sqrtDiscriminant = sqrt(discriminant);
+            return std::vector<double> {
+                    (-b + sqrtDiscriminant) / (2 * a),
+                    (-b - sqrtDiscriminant) / (2 * a)
+            };
+        } else if(discriminant == 0) { // 1 real root
+            return std::vector<double> {
+                    -b / (2 * a)
+            };
+        }
+
+        return std::vector<double> {};
+    }
 };
 
 #endif //EXT_POCKETMINE_MATH_MATH_H
