a43c9aae3693fb0256be7dd529a8591b24479339
Jack Noordhuis <daswaggamer@gmail.com>
2018-11-01 17:08:26 +1100

    Implement matrix class

--- lib/Matrix.h
+++ lib/Matrix.h
@@ -6,51 +6,184 @@
 #define EXT_POCKETMINE_MATH_MATRIX_H
 
 #include <map>
+#include <stdexcept>
+#include <string>
 
 class Matrix {
 private:
-    std::map<int, int> matrix;
+    std::map<int, std::map<int, int>> matrix;
     int rows = 0;
     int columns = 0;
 public:
 
-    bool offsetExists(int offset);
-
-    int offsetGet(int offset);
-
-    void offsetSet(int offset, int value);
-
-    void offsetUnset(int offset);
-
-    Matrix(int rows, int columns, std::map<int, int> *set) {
-        Matrix::rows = rows;
-        Matrix::columns = columns;
-        Matrix::set(set);
+    bool offsetExists(int offset) {
+        return matrix.find(offset) == matrix.end();
     }
 
-    void set(std::map<int, int> *m);
+    std::map<int, int> *offsetGet(int offset) {
+        return &matrix.at(offset);
+    }
 
-    int getRows();
+    void offsetSet(int offset, std::map<int, int> *value) {
+        matrix[offset] = *value;
+    }
 
-    int getColumns();
+    void offsetUnset(int offset) {
+        matrix.erase(offset);
+    }
 
-    bool setElement(int row, int column, int value);
+    Matrix(int rows, int columns, std::map<int, std::map<int, int>> *set = nullptr) {
+        this->rows = rows;
+        this->columns = columns;
+        this->matrix = *set;
+    }
 
-    int *getElement(int row, int column);
+    /**
+     * Create a clone of an existing AxisAlignedBB.
+     *
+     * @param old The matrix to clone from.
+     */
+    explicit Matrix(const Matrix* old) {
+        rows = old->rows;
+        columns = old->columns;
+        matrix = old->matrix;
+    }
 
-    bool isSquare();
+    void set(std::map<int, std::map<int, int>> *m) {
+        matrix = *m;
+    }
 
-    Matrix *add(const Matrix *matrix);
+    int getRows() {
+        return rows;
+    }
 
-    Matrix *subtract(const Matrix *matrix);
+    int getColumns() {
+        return columns;
+    }
 
-    Matrix *multiplyScalar(const Matrix *matrix);
+    bool setElement(int row, int column, int value) {
+        if(row > this->rows or row < 0 or column > this->columns or column < 0) {
+            return false;
+        }
+        std::map<int, int> v;
+        matrix[row][column] = value;
 
-    Matrix *divideScalar(const Matrix *matrix);
+        return true;
+    }
 
-    Matrix *transpose(const Matrix *matrix);
+    int *getElement(int row, int column) {
+        if(row > this->rows or row < 0 or column > this->columns or column < 0) {
+            return nullptr;
+        }
 
-    Matrix *determinant(const Matrix *matrix);
+        return &matrix.at(row).at(column);
+    }
+
+    bool isSquare() {
+        return rows == columns;
+    }
+
+    Matrix *add(Matrix *matrix) {
+        if(rows != matrix->getRows() or columns != matrix->getColumns()) {
+            return nullptr;
+        }
+
+        auto *result = new Matrix(rows, columns);
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < columns; ++c) {
+                result->setElement(r, c, *getElement(r, c) + *matrix->getElement(r, c));
+            }
+        }
+
+        return result;
+    }
+
+    Matrix *subtract(Matrix *matrix) {
+        if(rows != matrix->getRows() or columns != matrix->getColumns()) {
+            return nullptr;
+        }
+
+        auto result = new Matrix(rows, columns);
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < columns; ++c) {
+                result->setElement(r, c, *getElement(r, c) - *matrix->getElement(r, c));
+            }
+        }
+
+        return result;
+    }
+
+    Matrix *multiplyScalar(int number) {
+        auto *result = new Matrix(this);
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < columns; ++c) {
+                result->setElement(r, c, *getElement(r, c) * number);
+            }
+        }
+
+        return result;
+    }
+
+    Matrix *divideScalar(int number) {
+        auto *result = new Matrix(this);
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < columns; ++c) {
+                result->setElement(r, c, *getElement(r, c) / number);
+            }
+        }
+
+        return result;
+    }
+
+    Matrix *transpose(Matrix *matrix) {
+        auto result = new Matrix(rows, columns);
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < columns; ++c) {
+                result->setElement(r, c, *getElement(r, c));
+            }
+        }
+
+        return result;
+    }
+
+    //Naive Matrix product, O(n^3)
+    Matrix *product(Matrix *matrix) {
+        if(columns != matrix->getRows()) {
+            return nullptr;
+        }
+
+        int c = matrix->getColumns();
+        auto result = new Matrix(rows, c);
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0; j < c; ++j) {
+                int sum = 0;
+                for(int k = 0; k < columns; ++k) {
+                    sum += *getElement(i, k) * *matrix->getElement(k, j);
+                }
+                result->setElement(i, j, sum);
+            }
+        }
+
+        return result;
+    }
+
+    //Computation of the determinant of 2x2 and 3x3 matrices
+    int *determinant() {
+        if(!isSquare()) {
+            return nullptr;
+        }
+
+        switch(rows) {
+            case 1:
+                return new int(0);
+            case 2:
+                return new int(*getElement(0, 0) * *getElement(1, 1) - *getElement(0, 1) * *getElement(1, 0));
+            case 3:
+                return new int(*getElement(0, 0) * *getElement(1, 1) * *getElement(2, 2) + *getElement(0, 1) * *getElement(1, 2) * *getElement(2, 0) + *getElement(0, 2) * *getElement(1, 0) * *getElement(2, 1) - *getElement(2, 0) * *getElement(1, 1) * *getElement(0, 2) - *getElement(2, 1) * *getElement(1, 2) * *getElement(0, 0) - *getElement(2, 2) * *getElement(1, 0) * *getElement(0, 1));
+        }
+
+        return nullptr;
+    }
 };
 
 #endif //EXT_POCKETMINE_MATH_MATRIX_H
